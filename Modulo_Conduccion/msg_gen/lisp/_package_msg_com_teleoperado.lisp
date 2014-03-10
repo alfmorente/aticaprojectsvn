60cb690a728a40cee176acf201b1f2f31441e2db
alfmorente <alfmorente@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2014-03-10 11:06:41 +0000

    Borrado version anterior Conduccion

--- Modulo_Conduccion/msg_gen/lisp/_package_msg_com_teleoperado.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package Modulo_Conduccion-msg)
-(cl:export '(ID_ELEMENTO-VAL
-          ID_ELEMENTO
-          VALOR-VAL
-          VALOR
-          DEPURADO-VAL
-          DEPURADO
-))
--- Modulo_Conduccion/src/Modulo_Conduccion/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._msg_com_teleoperado import *
-from ._msg_errores import *
-from ._msg_gest_navegacion import *
