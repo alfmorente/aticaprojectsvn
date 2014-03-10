2479501a6c7db7a40fb2f5e0677b2e19fe228090
alfmorente@gmail.com <alfmorente@gmail.com@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2014-03-10 09:08:19 +0000

    Teleoperacion.h cambios

--- Modulo_Teleoperacion/include/Modulo_Teleoperacion/teleoperacion.h
+++ Modulo_Teleoperacion/include/Modulo_Teleoperacion/teleoperacion.h
@@ -37,15 +37,30 @@ extern "C" {
 ros::NodeHandle n;
 bool enableModule;
 int error_count;
-int LasFront_warning;
-int LasFront_alarm;
+
 
 //Funciones de suscripcion
 void fcn_sub_com_teleop(const Modulo_Teleoperacion::msg_com_teleop);
 void fcn_sub_enable_module(const Modulo_Teleoperacion::msg_module_enable);
-void fcn_sub_laser(const Modulo_Teleoperacion::msg_laser);
 
 //Funciones propias
 int convertToCorrectValues(int,int);
 // Devuelve true si el proccesado de datos dictamina peligro y false en caso contrario
 short processDataLaser(Modulo_Teleoperacion::msg_laser);
+
+#define MAX_OUTRANGE_ERROR 10
+// Valores máximos y mínimos de los comandos de teleoperación
+#define MIN_STEER_VALUE -100
+#define MAX_STEER_VALUE 100
+#define MIN_THROTTLE_VALUE 0
+#define MAX_THROTTLE_VALUE 100
+#define MIN_BRAKE_VALUE 0
+#define MAX_BRAKE_VALUE 100
+#define MIN_HANDBRAKE_VALUE 0
+#define MAX_HANDBRAKE_VALUE 1
+#define MIN_GEAR_VALUE 0
+#define MAX_GEAR_VALUE 4
+#define MIN_LIGHT_VALUE 0
+#define MAX_LIGHT_VALUE 1
+#define MIN_LIGHTS_IR_VALUE 0
+#define MAX_LIGHTS_IR_VALUE 1
