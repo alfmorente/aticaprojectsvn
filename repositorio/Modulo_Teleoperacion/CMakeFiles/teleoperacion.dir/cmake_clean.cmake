3c3eb68ef43b4c36947d9520eb20514da4f7a904
alfmorente <alfmorente@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2014-03-13 12:41:52 +0000

    Resubida REMOTE

---
+++ repositorio/Modulo_Teleoperacion/CMakeFiles/teleoperacion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/teleoperacion.dir/src/teleoperacion.cpp.o"
+  "bin/teleoperacion.pdb"
+  "bin/teleoperacion"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/teleoperacion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
