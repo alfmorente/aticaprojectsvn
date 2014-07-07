a10eb1cc11911e6042331b503370040d9911eb4c
caramoalv@gmail.com <caramoalv@gmail.com@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2014-07-07 09:59:24 +0000

    Primera  version

---
+++ CITIUS/Driver_TRAX_Mag/constant.h
@@ -0,0 +1,63 @@
+/* 
+ * File:   constant.h
+ * Author: atica
+ *
+ * Created on 1 de julio de 2014, 17:56
+ */
+
+#ifndef CONSTANT_H
+#define	CONSTANT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CONSTANT_H */
+
+#include <iostream>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <complex>
+
+
+typedef struct {
+    unsigned char len;
+    unsigned char *payload;
+    unsigned char cs;
+} TraxMsg;
+
+typedef struct {
+    unsigned char idData;
+    unsigned char Value;
+} PayloadData;
+
+
+
+
+void sendToDevice(TraxMsg);
+void waitForAck(unsigned char);
+unsigned char calcChecksum(TraxMsg);
+bool isCheckSumOK(TraxMsg);
+TraxMsg kGetData();
+
+
+float hexa2float(unsigned char * );
+double hexa2double(unsigned char * );
+int hexa2int(unsigned char * );
+
+
+
+struct termios newtio, oldtio;
+int canal;
+
---
+++ CITIUS/Driver_TRAX_Mag/main.cpp
@@ -0,0 +1,202 @@
+/* 
+ * File:   main.cpp
+ * Author: atica
+ *
+ * Created on 1 de julio de 2014, 9:55
+ */
+
+#include "constant.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    char * serial_name = (char *) "/dev/ttyUSB3";
+
+    canal = open(serial_name, O_RDWR | O_NOCTTY | O_NDELAY);
+
+    if (canal < 0) {
+
+        perror(serial_name);
+        return -1;
+
+    } else {
+
+        tcgetattr(canal, &oldtio);
+        bzero(&newtio, sizeof (newtio));
+        newtio.c_cflag = B38400 | CRTSCTS | CS8 | CLOCAL | CREAD;
+
+        newtio.c_iflag &= ~(IGNBRK | BRKINT | ICRNL | INLCR | PARMRK | INPCK | ISTRIP | IXON);
+
+        newtio.c_oflag = 0;
+
+        newtio.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
+
+        newtio.c_cc[VINTR] = 0; /* Ctrl-c */
+        newtio.c_cc[VQUIT] = 0; /* Ctrl-\ */
+        newtio.c_cc[VERASE] = 0; /* del */
+        newtio.c_cc[VKILL] = 0; /* @ */
+        newtio.c_cc[VEOF] = 4; /* Ctrl-d */
+        newtio.c_cc[VTIME] = 0; /* temporizador entre caracter, no usado */
+        newtio.c_cc[VMIN] = 1; /* bloqu.lectura hasta llegada de caracter. 1 */
+        newtio.c_cc[VSWTC] = 0; /* '\0' */
+        newtio.c_cc[VSTART] = 0; /* Ctrl-q */
+        newtio.c_cc[VSTOP] = 0; /* Ctrl-s */
+        newtio.c_cc[VSUSP] = 0; /* Ctrl-z */
+        newtio.c_cc[VEOL] = 0; /* '\0' */
+        newtio.c_cc[VREPRINT] = 0; /* Ctrl-r */
+        newtio.c_cc[VDISCARD] = 0; /* Ctrl-u */
+        newtio.c_cc[VWERASE] = 0; /* Ctrl-w */
+        newtio.c_cc[VLNEXT] = 0; /* Ctrl-v */
+        newtio.c_cc[VEOL2] = 0; /* '\0' */
+
+        tcflush(canal, TCIFLUSH);
+        tcsetattr(canal, TCSANOW, &newtio);
+        
+        sendToDevice(kGetData());
+;
+
+    }
+    return 0;
+}
+
+
+
+void sendToDevice(TraxMsg msg) {
+    unsigned char *msg2send = (unsigned char *) malloc(msg.len);
+
+    msg2send[0] = msg.len;
+
+    if (msg.len > 0) {
+        for (int i = 0; i < msg.len; i++) {
+            msg2send[i + 2] = msg.payload[i];
+        }
+    }
+
+    msg2send[msg.len - 2] = msg.cs;
+    
+    printf("Sent: ");
+    for(int i=0;i< (msg.len + 5);i++){
+        printf("%02X ",msg2send[i]&0xFF);
+    }
+    printf("\n");
+    
+    write(canal, msg2send, msg.len + 5); // Enviamos el comando para que comience a enviarnos datos
+    waitForAck(0);
+
+}
+
+void waitForAck(unsigned char _mid) {
+
+    unsigned char byte;
+    bool ackFound = false;    
+    int cuentavieja = 0;
+
+    while (!ackFound) {
+
+        if (read(canal, &byte, 1) > 0) {
+            printf("%02X ", byte);
+        }
+        cuentavieja++;
+        if(cuentavieja == 10)
+            ackFound = true;
+    }
+}
+
+unsigned char calcChecksum(TraxMsg msg) {
+    unsigned char cs = 0;
+
+    cs += msg.bid;
+    cs += msg.mid;
+    cs += msg.len;
+
+    if (msg.len > 0) {
+        for (int i = 0; i < msg.len; i++) {
+            cs += msg.data[i];
+        }
+    }
+    return 0x00 - cs;
+}
+
+// Comprobacion del checksum
+
+bool isCheckSumOK(TraxMsg msg) {
+    unsigned char cs = 0;
+    cs += msg.bid;
+    cs += msg.mid;
+    cs += msg.len;
+
+    if (msg.len > 0) {
+        for (int i = 0; i < msg.len; i++) {
+            cs += msg.data[i];
+        }
+    }
+    cs += msg.cs;
+
+    return cs == 0x00;
+}
+
+// GetDeviceID
+
+TraxMsg kGetData() {
+    TraxMsg xsMsg;
+    xsMsg.pre = COMMAND_PRE;
+    xsMsg.bid = COMMAND_BID;
+    xsMsg.mid = COMMAND_MID_REQDID;
+    xsMsg.len = COMMAND_LEN_0;
+    xsMsg.cs = calcChecksum(xsMsg);
+    return xsMsg;
+}
+
+float hexa2float(unsigned char * buffer){
+    union
+    {
+        float value;
+        unsigned char buffer[4];
+
+    }floatUnion;
+
+    floatUnion.buffer[0] = buffer[3];
+    floatUnion.buffer[1] = buffer[2];
+    floatUnion.buffer[2] = buffer[1];
+    floatUnion.buffer[3] = buffer[0];
+
+    return floatUnion.value;
+}
+
+int hexa2int(unsigned char * buffer){
+    union
+    {
+        int value;
+        unsigned char buffer[4];
+
+    }floatUnion;
+
+    floatUnion.buffer[0] = buffer[3];
+    floatUnion.buffer[1] = buffer[2];
+    floatUnion.buffer[2] = buffer[1];
+    floatUnion.buffer[3] = buffer[0];
+
+    return floatUnion.value;
+}
+
+double hexa2double(unsigned char * buffer){
+    union{
+        double value;
+        unsigned char buffer[8];
+    }floatUnion;
+
+    floatUnion.buffer[0] = buffer[7];
+    floatUnion.buffer[1] = buffer[6];
+    floatUnion.buffer[2] = buffer[5];
+    floatUnion.buffer[3] = buffer[4];
+    floatUnion.buffer[4] = buffer[3];
+    floatUnion.buffer[5] = buffer[2];
+    floatUnion.buffer[6] = buffer[1];
+    floatUnion.buffer[7] = buffer[0];
+
+    return floatUnion.value;
+}
