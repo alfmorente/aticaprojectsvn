a47ceed8d53c1625f85398236d6d70a6f91068d5
caramoalv@gmail.com <caramoalv@gmail.com@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2014-07-15 10:52:07 +0000

    Proyecto vacio

---
+++ CITIUS/CITIUS_Control_Manager/CMakeFiles/ROSBUILD_genmanifest_roseus_CITIUS_Control_Manager.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ROSBUILD_genmanifest_roseus_CITIUS_Control_Manager"
+  "/home/atica/.ros/roseus/groovy/CITIUS_Control_Manager/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_genmanifest_roseus_CITIUS_Control_Manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
