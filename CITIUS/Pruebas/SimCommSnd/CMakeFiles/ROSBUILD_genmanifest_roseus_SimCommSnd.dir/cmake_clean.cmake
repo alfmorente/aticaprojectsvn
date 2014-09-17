2696699407c65095a41b63e165e2913814e7bbcf
alfmorente <alfmorente@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2014-09-17 11:29:42 +0000

    Borrado

--- CITIUS/Pruebas/SimCommSnd/CMakeFiles/ROSBUILD_genmanifest_roseus_SimCommSnd.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/ROSBUILD_genmanifest_roseus_SimCommSnd"
-  "/home/atica/.ros/roseus/groovy/SimCommSnd/manifest.l"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/ROSBUILD_genmanifest_roseus_SimCommSnd.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
