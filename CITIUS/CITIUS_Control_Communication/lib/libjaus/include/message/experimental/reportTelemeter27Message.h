8e462038aa7877ecd92602df59428a620cbbc719
caramoalv@gmail.com <caramoalv@gmail.com@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2014-09-19 11:34:21 +0000

    Cambios en configuracion
    Falta generar mensajes 
    
    26: setTelemeter26Message
    y 
    27: reportTelemeter27Message

---
+++ CITIUS/CITIUS_Control_Communication/lib/libjaus/include/message/experimental/reportTelemeter27Message.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   reportTelemeter27Message.h
+ * Author: atica
+ *
+ * Created on 19 de septiembre de 2014, 13:24
+ */
+
+#ifndef REPORTTELEMETER27MESSAGE_H
+#define	REPORTTELEMETER27MESSAGE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* REPORTTELEMETER27MESSAGE_H */
+
---
+++ CITIUS/CITIUS_Control_Communication/lib/libjaus/include/message/experimental/setTelemeter26Message.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   setTelemeter26Message.h
+ * Author: atica
+ *
+ * Created on 19 de septiembre de 2014, 13:24
+ */
+
+#ifndef SETTELEMETER26MESSAGE_H
+#define	SETTELEMETER26MESSAGE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SETTELEMETER26MESSAGE_H */
+
