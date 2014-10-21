4c69bd77376cba3290a4419914941ff70ee0513e
caramoalv@gmail.com <caramoalv@gmail.com@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2014-10-21 10:51:54 +0000

    Primera versión de SimElectricSnd como copia de SimElectricRcv

---
+++ CITIUS/Pruebas/SimElectricSnd/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=simelectricsnd
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/simelectricsnd
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=simelectricsnd.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/simelectricsnd.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=simelectricsnd
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/simelectricsnd
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=simelectricsnd.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/simelectricsnd.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
