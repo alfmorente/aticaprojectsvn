eb2e0bbc76b607fd74a18c71611b365ca27a0804
caramoalv@gmail.com <caramoalv@gmail.com@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2014-11-21 09:47:02 +0000

    Limpieza de codigo, comentarios, etc.

--- CITIUS/CITIUS_Control_Manager/src/main.cpp
+++ CITIUS/CITIUS_Control_Manager/src/main.cpp
@@ -16,16 +16,10 @@
  * @return Entero distinto de 0 si ha habido problemas. 0 en caso contrario.
  */
 int main(int argc, char** argv) {
-
   ros::init(argc, argv, "Control_ROS_Node_Manager");
-
-  // Creacion de la maquina de estados
   Manager *manager = new Manager();
-
-  // Iniciar artefactos ROS
   manager->initROS();
   ROS_INFO("[Control] Manager - Nodo listo para operar");
-
   int status = -1;
   ros::NodeHandle nh;
   nh.setParam("vehicleStatus", status);
@@ -35,17 +29,13 @@ int main(int argc, char** argv) {
     ros::spinOnce();
   }
   ROS_INFO("[Control] Manager - Maquina de estados iniciada");
-
-  // Bucle principal
   while (ros::ok() && status != OPERATION_MODE_APAGANDO) {
     nh.getParam("vehicleStatus", status);
     ros::spinOnce();
     usleep(100000);
   }
-
   delete(manager);
   ROS_INFO("[Control] Manager - Nodo finalizado");
-
   return 0;
 }
 
