1da316892b2a503244e4106d3714ff61bbcb3f73
sergio.doctor.lopez@gmail.com <sergio.doctor.lopez@gmail.com@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2015-03-10 08:54:10 +0000

    Simulador Bobcat

---
+++ SimBobcat/constant.h
@@ -0,0 +1,170 @@
+/* 
+ * File:   constant.h
+ * Author: Carlos Amores
+ *
+ * Created on 4 de junio de 2014, 17:27
+ */
+
+#ifndef CONSTANT_H
+#define	CONSTANT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+/*******************************************************************************
+ * PROTOCOLO PAYLOAD DE CONDUCCION             
+ * IDENTIFICADOR DE INSTRUCCION
+*******************************************************************************/
+
+#define SET 0
+#define GET 1
+#define INFO 2
+#define ACK 3
+#define NACK 4
+
+
+/*******************************************************************************
+ * PROTOCOLO PAYLOAD DE CONDUCCION             
+ * IDENTIFICADOR DE INSTRUCCION
+*******************************************************************************/
+
+/**
+ * \enum DeviceID
+ * \brief Identificador de dispositivo para protocolo con Payload de conducción
+ */
+typedef enum {
+  RESET = 0, ///<Valor para formación de comando RESET
+  BLINKER_RIGHT = 1, ///<Valor para formación de comando BLINKER_RIGHT
+  BLINKER_LEFT = 2, ///<Valor para formación de comando BLINKER_LEFT
+  BLINKER_EMERGENCY = 3, ///<Valor para formación de comando BLINKER_EMERGENCY
+  MT_BLINKERS = 4, ///<Valor para formación de comando MT_BLINKERS
+  DIPSP = 5, ///<Valor para formación de comando DIPSP
+  DIPSS = 6, ///<Valor para formación de comando DIPSS
+  DIPSR = 7, ///<Valor para formación de comando DIPSR
+  KLAXON = 8, ///<Valor para formación de comando KLAXON
+  MT_LIGHTS = 9, ///<Valor para formación de comando MT_LIGHTS
+  GEAR = 10, ///<Valor para formación de comando GEAR
+  MT_GEAR = 11, ///<Valor para formación de comando MT_GEAR
+  THROTTLE = 12, ///<Valor para formación de comando THROTTLE
+  MOTOR_RPM = 13, ///<Valor para formación de comando MOTOR_RPM
+  CRUISING_SPEED = 14, ///<Valor para formación de comando CRUISING_SPEED
+  MT_THROTTLE = 15, ///<Valor para formación de comando MT_THROTTLE
+  MOTOR_TEMPERATURE = 16, ///<Valor para formación de comando MOTOR_TEMPERATURE
+  HANDBRAKE = 17, ///<Valor para formación de comando HANDBRAKE
+  MT_HANDBRAKE = 18, ///<Valor para formación de comando MT_HANDBRAKE
+  BRAKE = 19, ///<Valor para formación de comando BRAKE
+  MT_BRAKE = 20, ///<Valor para formación de comando MT_BRAKE
+  STEERING = 21, ///<Valor para formación de comando STEERING
+  STEERING_ALARMS = 22, ///<Valor para formación de comando STEERING_ALARMS
+  MT_STEERING = 23, ///<Valor para formación de comando MT_STEERING
+  DRIVE_ALARMS = 24, ///<Valor para formación de comando DRIVE_ALARMS
+  BATTERY_LEVEL = 25, ///<Valor para formación de comando BATTERY_LEVEL
+  BATTERY_VOLTAGE = 26, ///<Valor para formación de comando BATTERY_VOLTAGE
+  BATTERY_CURRENT = 27, ///<Valor para formación de comando BATTERY_CURRENT
+  BATTERY_TEMPERATURE = 28, ///<Valor para formación de comando BATTERY_TEMPERATURE
+  SUPPLY_TURN_ON = 29, ///<Valor para formación de comando SUPPLY_TURN_ON
+  SUPPLY_CHECK = 30, ///<Valor para formación de comando SUPPLY_CHECK
+  TURN_OFF = 31, ///<Valor para formación de comando TURN_OFF
+  SUPPLY_5 = 32, ///<Valor para formación de comando SUPPLY_5
+  SUPPLY_12 = 33, ///<Valor para formación de comando SUPPLY_12
+  SUPPLY_24_DRIVE = 34, ///<Valor para formación de comando SUPPLY_24_DRIVE
+  SUPPLY_24_OCC = 35, ///<Valor para formación de comando SUPPLY_24_OCC
+  SUPPLY_48 = 36, ///<Valor para formación de comando SUPPLY_48
+  CONTROL_SYSTEM_SUPPLY = 37, ///<Valor para formación de comando CONTROL_SYSTEM_SUPPLY
+  CONTROL_SYSTEM_SUPPLY_5 = 38, ///<Valor para formación de comando CONTROL_SYSTEM_SUPPLY_5
+  CONTROL_SYSTEM_SUPPLY_12 = 39, ///<Valor para formación de comando CONTROL_SYSTEM_SUPPLY_12
+  CONTROL_SYSTEM_SUPPLY_24 = 40, ///<Valor para formación de comando CONTROL_SYSTEM_SUPPLY_24
+  CONTROL_SYSTEM_SUPPLY_48 = 41, ///<Valor para formación de comando CONTROL_SYSTEM_SUPPLY_48
+  DRIVE_SYSTEM_SUPPLY = 42, ///<Valor para formación de comando DRIVE_SYSTEM_SUPPLY
+  DRIVE_SYSTEM_SUPPLY_5 = 43, ///<Valor para formación de comando DRIVE_SYSTEM_SUPPLY_5
+  DRIVE_SYSTEM_SUPPLY_12 = 44, ///<Valor para formación de comando DRIVE_SYSTEM_SUPPLY_12
+  DRIVE_SYSTEM_SUPPLY_24 = 45, ///<Valor para formación de comando DRIVE_SYSTEM_SUPPLY_24
+  DRIVE_SYSTEM_SUPPLY_48 = 46, ///<Valor para formación de comando DRIVE_SYSTEM_SUPPLY_48
+  COMM_SYSTEM_SUPPLY = 47, ///<Valor para formación de comando COMM_SYSTEM_SUPPLY
+  COMM_SYSTEM_SUPPLY_5 = 48, ///<Valor para formación de comando COMM_SYSTEM_SUPPLY_5
+  COMM_SYSTEM_SUPPLY_12 = 49, ///<Valor para formación de comando COMM_SYSTEM_SUPPLY_12
+  COMM_SYSTEM_SUPPLY_24 = 50, ///<Valor para formación de comando COMM_SYSTEM_SUPPLY_24
+  COMM_SYSTEM_SUPPLY_48 = 51, ///<Valor para formación de comando COMM_SYSTEM_SUPPLY_48
+  OBSERVATION_SYSTEM_SUPPLY = 52, ///<Valor para formación de comando OBSERVATION_SYSTEM_SUPPLY
+  OBSERVATION_SYSTEM_SUPPLY_5 = 53, ///<Valor para formación de comando OBSERVATION_SYSTEM_SUPPLY_5
+  OBSERVATION_SYSTEM_SUPPLY_12 = 54, ///<Valor para formación de comando OBSERVATION_SYSTEM_SUPPLY_12
+  OBSERVATION_SYSTEM_SUPPLY_24 = 55, ///<Valor para formación de comando OBSERVATION_SYSTEM_SUPPLY_24
+  OBSERVATION_SYSTEM_SUPPLY_48 = 56, ///<Valor para formación de comando OBSERVATION_SYSTEM_SUPPLY_48
+  SUPPLY_ALARMS = 57, ///<Valor para formación de comando SUPPLY_ALARMS
+  OPERATION_MODE_SWITCH = 58 ///<Valor para formación de comando OPERATION_MODE_SWITCH
+} DeviceID;
+
+
+
+/*******************************************************************************
+ *                              ESTADOS DEL NODO
+*******************************************************************************/
+
+#define NODESTATUS_INIT 0
+#define NODESTATUS_OK 1
+#define NODESTATUS_CORRUPT 2
+#define NODESTATUS_OFF 3
+
+/*******************************************************************************
+ *                          MODOS DE OPERACION DEL VEHICULO
+*******************************************************************************/
+
+#define OPERATION_MODE_LOCAL 0
+#define OPERATION_MODE_INICIANDO 1
+#define OPERATION_MODE_CONDUCCION 2
+#define OPERATION_MODE_OBSERVACION 3
+#define OPERATION_MODE_APAGANDO 4
+
+/*******************************************************************************
+ *                   SOCKET PAYLOAD DE CONDUCCION
+*******************************************************************************/
+
+#define IP_PAYLOAD_CONDUCCION_DRIVING "127.0.0.1"
+#define PORT_PAYLOAD_CONDUCCION_DRIVING 10000
+
+/*******************************************************************************
+ * FRECUENCIA DE REQUERIMIENTO DE INFORMACION A DISPOSITIVO
+ ******************************************************************************/
+
+#define FREC_10HZ 0.1
+#define FREC_5HZ 0.2
+#define FREC_2HZ 0.5
+#define FREC_1HZ 1
+
+/*******************************************************************************
+ *           ESTRUCTURA DE INTERCAMBIO CON PAYLOAD DE CONDUCCION
+*******************************************************************************/
+
+typedef struct{
+    short instruction;
+    short id_instruction;
+    short element;
+    short value;
+}FrameDriving;
+
+
+/*******************************************************************************
+ *           MÁSCARAS DE IDENTIFICACIÓN DE ALARMAS
+ *******************************************************************************/
+
+#define MASK_NOT_ALARMS 0x0000 ///<Indicador de todas las alarmas a 0
+#define MASK_ALARMS_CONNECTION_STEERING_FAILED 0x0001 ///<Indicador de fallo de conexión
+#define MASK_ALARMS_STEERING_FAILED 0x0002 ///<Indicador de fallo en la dirección
+#define MASK_ALARMS_BRAKE_CONNECTION_FAILED 0x0004 ///<Indicador de fallo de conexión con freno de servicio
+#define MASK_ALARMS_BRAKE_FAILED 0x0008 ///<Indicador de fallo de freno de servicio
+#define MASK_ALARMS_HANDBRAKE_CONNECTION_FALED 0x0010 ///<Indicador de fallo de conexión con freno de estacionamiento
+#define MASK_ALARMS_HANDBRAKE_FAILED 0x0020 ///<Indicador de fallo de freno de estacionamiento
+#define MASK_ALARMS_MOTOR_TEMPERATURE 0x0040 ///<Indicador de alta temperatura de motor
+#define MASK_ALARMS_FLAGS_FAILED 0x0080 ///< Indicador de fallo en los testigos
+#define MASK_ALARMS_ACC_FAILED 0x0100 ///<Indicador de fallo en aceleración
+#define MASK_ALARMS_GEAR_FAILED 0x0200 ///<Indicador de fallo en el cambio de marchas
+
+#endif	/* CONSTANT_H */
---
+++ SimBobcat/mainServer.cpp
@@ -0,0 +1,228 @@
+/* 
+ * File:   main.cpp
+ * Author: atica
+ *
+ * Created on 19 de junio de 2014, 10:05
+ */
+
+#include <stdio.h>          
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "constant.h"
+
+#define PORT 10000 /* El puerto que será abierto */
+#define BACKLOG 1 /* El número de conexiones permitidas */
+
+#define MAXDATASIZE 100   
+
+#include "constant.h"
+#include "MessageDispatcher.h"
+
+/*******************************************************************************
+    *               PROTOTIPOS DE FUNCIONES SECUNDARIAS
+*******************************************************************************/
+
+int printfMenu();
+void requestDispatcher(int socketDispatcher, int request);
+
+int main(int argc, char *argv[]) {
+    
+    int fd, fd2, currentMsgCount = 1; /* los ficheros descriptores */
+
+    struct sockaddr_in server;
+    /* para la información de la dirección del servidor */
+
+    struct sockaddr_in client;
+    /* para la información de la dirección del cliente */
+
+    socklen_t sin_size;
+
+    /* A continuación la llamada a socket() */
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("error en socket()\n");
+        exit(-1);
+    }
+
+    server.sin_family = AF_INET;
+
+    server.sin_port = htons(PORT);
+    /* ¿Recuerdas a htons() de la sección "Conversiones"? =) */
+
+    server.sin_addr.s_addr = INADDR_ANY;
+    /* INADDR_ANY coloca nuestra dirección IP automáticamente */
+
+    bzero(&(server.sin_zero), 8);
+    /* escribimos ceros en el reto de la estructura */
+
+    char buf[MAXDATASIZE];
+    /* en donde es almacenará el texto recibido */
+
+    /* A continuación la llamada a bind() */
+    if (bind(fd, (struct sockaddr*) &server, sizeof (struct sockaddr)) == -1) {
+        printf("Error en bind() \n");
+        exit(-1);
+    }
+
+    if (listen(fd, BACKLOG) == -1) { /* llamada a listen() */
+        printf("Error en listen()\n");
+        exit(-1);
+    }
+    sin_size = sizeof (struct sockaddr_in);
+    printf("Esperando conexion de RosNode_Driving\n");
+    /* A continuación la llamada a accept() */
+    if ((fd2 = accept(fd, (struct sockaddr *) &client, &sin_size)) == -1) {
+        printf("Error en accept()\n");
+        exit(-1);
+    }
+
+    printf("Se obtuvo una conexión desde %s\n", inet_ntoa(client.sin_addr));
+    /* que mostrará la IP del cliente */
+    
+    bool exit = false;
+    
+    int newSt = 0;
+
+  while (!exit) {
+
+    newSt = printfMenu();
+    if (newSt == 26) { // Salir
+      exit = true;
+    } else { // Opcion valida
+      requestDispatcher(fd2, newSt);
+    }
+  }
+}
+
+int printfMenu() {
+
+  cout << "****************************************" << endl;
+  cout << "1 ) Enviar mensaje THROTTLE" << endl;
+  cout << "2 ) Enviar mensaje BRAKE" << endl;
+  cout << "3 ) Enviar mensaje HANDBRAKE" << endl;
+  cout << "4 ) Enviar mensaje STEERING" << endl;
+  cout << "5 ) Enviar mensaje GEAR" << endl;
+  cout << "6 ) Enviar mensaje STEERING ALARMS" << endl;
+  cout << "7 ) Enviar mensaje DRIVE ALARMS" << endl;
+  cout << "8 ) Enviar mensaje BLINKER LEFT" << endl;
+  cout << "9 ) Enviar mensaje BLINKER RIGHT" << endl;
+  cout << "10) Enviar mensaje EMERGENCY BLINKER" << endl;
+  cout << "11) Enviar mensaje DIPSS" << endl;
+  cout << "12) Enviar mensaje DIPSR" << endl;
+  cout << "13) Enviar mensaje DIPSP" << endl;
+  cout << "14) Enviar mensaje KLAXON" << endl;
+  cout << "15) Enviar mensaje MT THROTTLE" << endl;
+  cout << "16) Enviar mensaje MT BRAKE" << endl;
+  cout << "17) Enviar mensaje MT HANDBRAKE" << endl;
+  cout << "18) Enviar mensaje MT STEERING" << endl;
+  cout << "19) Enviar mensaje MT GEAR" << endl;
+  cout << "20) Enviar mensaje MT BLINKER" << endl;
+  cout << "21) Enviar mensaje MT LIGHTS" << endl;
+  cout << "22) Enviar mensaje MOTOR RPM" << endl;
+  cout << "23) Enviar mensaje MOTOR TEMPERATURE" << endl;
+  cout << "24) Enviar mensaje CRUISSING SPEED" << endl;
+  cout << "25) Enviar mensaje SWITCHER" << endl;
+  cout << "26) Salir" << endl;
+  cout << "****************************************" << endl;
+  cout << " Selecciona una opcion:" << endl;
+
+  int intAux;
+  cin >> intAux;
+
+  if (intAux < 1 || intAux > 26) {
+    cout << "Opcion no valida" << endl;
+    return 0;
+  } else {
+    return intAux;
+  }
+
+}
+
+void requestDispatcher(int socketDescriptor, int request) {
+  MessageDispatcher *disp = new MessageDispatcher();
+  switch (request) {
+    case 1: // throttle
+      disp->sendThrottleInfo(socketDescriptor);
+      break;
+    case 2: // brake
+      disp->sendBrakeInfo(socketDescriptor);
+      break;
+    case 3: // handbrake
+      disp->sendHandbrakeInfo(socketDescriptor);
+      break;
+    case 4: // steering
+      disp->sendSteeringInfo(socketDescriptor);
+      break;
+    case 5: // gear
+      disp->sendGearInfo(socketDescriptor);
+      break;
+    case 6: // steering alarms
+      disp->sendSteeringAlarmsInfo(socketDescriptor);
+      break;
+    case 7: // drive alarms
+      disp->sendDriveAlarmsInfo(socketDescriptor);
+      break;
+    case 8: // blinker left
+      disp->sendBlinkerLeftInfo(socketDescriptor);
+      break;
+    case 9: // blinker right
+      disp->sendBlinkerRightInfo(socketDescriptor);
+      break;
+    case 10: // emergency blinkers
+      disp->sendEmergencyBlinkerInfo(socketDescriptor);
+      break;
+    case 11: // dipss
+      disp->sendDipssInfo(socketDescriptor);
+      break;
+    case 12: // dipsr
+      disp->sendDipsrInfo(socketDescriptor);
+      break;
+    case 13: // dipsp
+      disp->sendDipspInfo(socketDescriptor);
+      break;
+    case 14: // klaxon
+      disp->sendKlaxonInfo(socketDescriptor);
+      break;
+    case 15: // mt_throttle
+      disp->sendMTThrottleInfo(socketDescriptor);
+      break;
+    case 16: // mt_brake
+      disp->sendMTBrakeInfo(socketDescriptor);
+      break;
+    case 17: // mt_handbrake
+      disp->sendMTHandbrakeInfo(socketDescriptor);
+      break;
+    case 18: // mr_steering
+      disp->sendMTSteeringInfo(socketDescriptor);
+      break;
+    case 19: // mt_gear
+      disp->sendMTGearInfo(socketDescriptor);
+      break;
+    case 20: // mt_blinker
+      disp->sendMTBlinkersInfo(socketDescriptor);
+      break;
+    case 21: // mt_lights
+      disp->sendMTLightsInfo(socketDescriptor);
+      break;
+    case 22: // motor rpm
+      disp->sendMotorRPMInfo(socketDescriptor);
+      break;
+    case 23: // motor temperature
+      disp->sendMotorTemperatureInfo(socketDescriptor);
+      break;
+    case 24: // cruissing speed
+      disp->sendCruissingSpeedInfo(socketDescriptor);
+      break;
+    case 25:
+      disp->sendSWitcherInfo(socketDescriptor);
+      break;
+    default: // nothing to do
+      break;
+
+
+  }
+}
