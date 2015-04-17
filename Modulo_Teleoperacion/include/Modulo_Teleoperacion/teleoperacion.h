092e134fe5a4ea5a7c15bf6e4f2b2eb9ac90ca05
alfmorente@gmail.com <alfmorente@gmail.com@7b7a857e-aeae-ed10-cb95-491734adc4e9>
2015-04-17 11:02:35 +0000

    Uso de doxygen en el módulo

--- Modulo_Teleoperacion/include/Modulo_Teleoperacion/teleoperacion.h
+++ Modulo_Teleoperacion/include/Modulo_Teleoperacion/teleoperacion.h
@@ -64,25 +64,25 @@ bool fcn_heartbeat(Common_files::srv_data::Request &req, Common_files::srv_data:
 void initialize(ros::NodeHandle n);
 int convertToCorrectValues(int,int);
 
-#define MAX_OUTRANGE_ERROR 3
+#define MAX_OUTRANGE_ERROR 3    ///<Número de errores en los comandos para mandar mensaje de error
 // Valores máximos y mínimos de los comandos de teleoperación
-#define MIN_STEER_VALUE -100
-#define MAX_STEER_VALUE 100
-#define MIN_THROTTLE_VALUE 0
-#define MAX_THROTTLE_VALUE 100
-#define MIN_BRAKE_VALUE 0
-#define MAX_BRAKE_VALUE 100
-#define MIN_HANDBRAKE_VALUE 0
-#define MAX_HANDBRAKE_VALUE 1
-#define MIN_GEAR_VALUE 0
-#define MAX_GEAR_VALUE 4
-#define MIN_LIGHT_VALUE 0
-#define MAX_LIGHT_VALUE 1
-#define MIN_LIGHT_IR_VALUE 0
-#define MAX_LIGHT_IR_VALUE 1
-#define MIN_ENGINE_VALUE 0
-#define MAX_ENGINE_VALUE 1
-#define MIN_DIFF_VALUE 0
-#define MAX_DIFF_VALUE 1
-#define MIN_LASER_VALUE 0
-#define MAX_LASER_VALUE 1
+#define MIN_STEER_VALUE -100    ///<Valor mínimo del comando dirección
+#define MAX_STEER_VALUE 100     ///<Valor máximo del comando dirección
+#define MIN_THROTTLE_VALUE 0    ///<Valor mínimo del comando acelerador
+#define MAX_THROTTLE_VALUE 100  ///<Valor máximo del comando acelerador
+#define MIN_BRAKE_VALUE 0       ///<Valor mínimo del comando freno
+#define MAX_BRAKE_VALUE 100     ///<Valor máximo del comando freno
+#define MIN_HANDBRAKE_VALUE 0   ///<Valor mínimo del comando freno de estacionamiento
+#define MAX_HANDBRAKE_VALUE 1   ///<Valor máximo del comando freno de estacionamiento
+#define MIN_GEAR_VALUE 0        ///<Valor mínimo del comando marcha
+#define MAX_GEAR_VALUE 4        ///<Valor máximo del comando marcha
+#define MIN_LIGHT_VALUE 0       ///<Valor mínimo del comando luz convencional
+#define MAX_LIGHT_VALUE 1       ///<Valor máximo del comando luz convencional
+#define MIN_LIGHT_IR_VALUE 0    ///<Valor mínimo del comando luz infrarroja
+#define MAX_LIGHT_IR_VALUE 1    ///<Valor máximo del comando luz infrarroja
+#define MIN_ENGINE_VALUE 0      ///<Valor mínimo del comando encendido de motor
+#define MAX_ENGINE_VALUE 1      ///<Valor máximo del comando encendido de motor
+#define MIN_DIFF_VALUE 0        ///<Valor mínimo del comando activación del diferencial
+#define MAX_DIFF_VALUE 1        ///<Valor máximo del comando activación del diferencial
+#define MIN_LASER_VALUE 0       ///<Valor mínimo del comando activación del laser
+#define MAX_LASER_VALUE 1       ///<Valor máximo del comando activación del laser
